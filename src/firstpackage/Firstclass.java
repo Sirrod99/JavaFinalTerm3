6a29e0fee437702f5934409491d2a89322a719d3
Chasek99 <ck121642@students.davenport.k12.ia.us>
2017-03-08 13:36:48 -0600

    update

--- src/firstpackage/Firstclass.java
+++ src/firstpackage/Firstclass.java
@@ -16,6 +16,8 @@ public class Firstclass {
 	public static void main(String[] args) 
 	{
 	int userInput;
+	int choiceYes;
+	int choiceNo;
 	
 	
 	
@@ -26,23 +28,24 @@ public class Firstclass {
 			+ " \nwith a surplus of resources, the population is fine. Due to your boredom and isolation, you wish to leave,\n and explore the wastes of western Nevada."
 			+ "  It is April of the year 2227,\n 150 years after nuclear fire destroyed the world.");
 	
-	//Name is prompted
-	
+
 	
 	
 	
 	//Weapons
-	JOptionPane.showMessageDialog(null, "Player grabs one weapon before he/she leaves. A 10mm pistol or a 10mm submachine gun,"
-			+ " \nWith 24 bullets The player also gets 3 healing items(stimpacks), a flashlight");
-	//Player chooses weapon(Choice)
+	JOptionPane.showMessageDialog(null, "Player grabs one weapon before he/she leaves. A 10mm pistol"
+			+ " \nWith 24 bullets, The player also gets 3 healing items(stimpacks), a flashlight");
+
 	
 	
+		
 	
 	//Resident tell you about a town.
-	JOptionPane.showMessageDialog(null, "One you the other citizens of the vault tell you about a town called Gecko to the north."
+	JOptionPane.showMessageDialog(null, "One of the other citizens of the vault tell you about a town called Gecko to the north."
 			+ " He smirks at you and wishes you luck on your travels.");
 	
 	//Three decisons are given(Choice)
+	
 	
 
 	
